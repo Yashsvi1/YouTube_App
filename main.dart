609c1a303eded515893e834f4ba6f17fce12d32d
Yashsvi <mauryayashsvi@gmail.com>
2023-07-26 10:37:13 +0530

    Add files via upload
    Main File

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '/pages/dashboard.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Dashboard(), 
+    theme: ThemeData.dark(),
+  debugShowCheckedModeBanner: false,));         //material app
+}
+
